45103c6a674c34895cea2c2fc7f70d91de057465
czops <37029225+czops@users.noreply.github.com>
2019-05-24 18:19:54 -0500

    most of the app structure done

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,11 @@
 import React, { Component } from "react";
+import { Jumbotron, Col, Row, Container } from "react-bootstrap";
+
+import SavedComponent from "./components/saved/Saved";
+import SearchComponent from "./components/search/Search";
+
+// import Search from "./src/components/search";
+// import Saved from "./src/components/saved";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -6,6 +13,27 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+
+        <Jumbotron fluid>
+          <Container>
+            <h1>(React) Google Book Search!</h1>
+            <p>
+              Search for and Save books of interest!
+    </p>
+          </Container>
+        </Jumbotron>
+
+        <Container>
+          <Row>
+            <Col>
+              <SearchComponent />
+              <SavedComponent />
+            </Col>
+          </Row>
+        </Container>
+
+
+
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -14,8 +42,48 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+
+
+
     );
   }
+
+  //Don't really think router is necessary for this application
+
+  // render() {
+  //   return (
+
+  //     <Router>
+  //       <div>
+  //         <Switch>
+  //           <Route exact path="/" component={Login} />
+  //           {/* <Route exact path="/LandingPage" render={(state) => <LandingPage {...state}/>} /> */}
+  //           <Route exact path="/Landing" render={(state) => <Landing 
+  //           masterState={this.state} 
+  //           setMasterState={this.setMasterState} 
+  //           setPanelNumber={this.setPanelNumber} 
+  //           setModelNumber={this.setModelNumber} 
+  //           setShopOrderNumber={this.setShopOrderNumber} 
+  //           setZone={this.setZone} 
+  //           setSize={this.setSize}
+  //           setMeasurement1={this.setMeasurement1} 
+  //           setMeasurement2={this.setMeasurement2}
+  //           />} />
+
+  //           <Route exact path="/Process" component={Process} />
+  //           <Route exact path="/Measurements" render={(state) => <Measurements masterState={this.state} setMasterState={this.setMasterState} />} />
+  //           <Route exact path="/Process1" render={(state) => <Process1 masterState={this.state} setMasterState={this.setMasterState} process1CheckState={this.process1CheckState} handleInputChange={this.handleInputChange} PUTsomeStuff={this.PUTsomeStuff} />} />
+  //           <Route exact path="/Process2" render={(state) => <Process2 masterState={this.state} setMasterState={this.setMasterState} />} />
+  //           <Route exact path="/Success" component={Success} />
+  //           {/* Create route from api to pull all panels, for T&E */}
+  //           <Route component={NotFound} />
+  //         </Switch>
+  //       </div>
+  //     </Router>
+  //   );
+  // }
+
+
 }
 
 export default App;
---
+++ client/src/components/saved/Saved.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+
+
+function Saved (props) {
+    return (
+        <div className="formBox">
+            
+            {/* <Form>
+            <Form.Text className="stepText">
+                STEP 3.
+            </Form.Text>
+            <Form.Text className="sizeTitle">
+                Enter the Measurements
+            </Form.Text>
+            {props.panels.map(panel => 
+                <Form.Row className="measureBox">
+                <Form.Group as={Col} controlId="formModelNumber">
+                    <Form.Text className="processTitle"> PANEL {panel.panelNumber} <span className="panelNumberEnter"></span></Form.Text>
+                    <Form.Row>
+                        <Form.Group>
+                            <Form.Label>Measurement 1</Form.Label>
+                            <Form.Control onChange={(event) => props.setMeasurement1(event, panel.panelNumber)} value={panel.m1} placeholder="Enter #" className="measurement1"/>
+                        </Form.Group>
+                        <Form.Group className="secondMeasure">
+                            <Form.Label>Measurement 2</Form.Label>
+                            <Form.Control onChange={(event) => props.setMeasurement2(event, panel.panelNumber)} value={panel.m2} placeholder="Enter #" className="measurement2"/>
+                        </Form.Group>
+                    
+                    </Form.Row>
+                    </Form.Group>
+                    </Form.Row>
+                )}
+         
+                <div className="buttonBox">
+                    <Button variant="primary" type="submit" className="submitButton" block>
+                        SUBMIT
+                    </Button>
+                </div>
+
+            </Form> */}
+        </div>
+    )
+}
+
+export default Saved;
