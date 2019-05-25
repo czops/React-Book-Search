2f324e1309b11139acabdb6844c4b914d6873693
czops <37029225+czops@users.noreply.github.com>
2019-05-25 12:50:58 -0500

    beginning to create the DB
    using mongoose- need to read about the Googlebooks API

--- models/bookModel.js
+++ models/bookModel.js
@@ -6,47 +6,15 @@ var Schema = mongoose.Schema;
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
 var BookSchema = new Schema({
-  // `username` must be of type String
-  // `username` will trim leading and trailing whitespace before it's saved
-  // `username` is a required field and throws a custom error message if not supplied
-  title: {
-    type: String,
-    trim: true,
-    required: "Title is Required"
-  },
-  // `password` must be of type String
-  // `password` will trim leading and trailing whitespace before it's saved
-  // `password` is a required field and throws a custom error message if not supplied
-  // `password` uses a custom validation function to only accept values 6 characters or more
-  password: {
-    type: String,
-    trim: true,
-    required: "Password is Required",
-    validate: [
-      function(input) {
-        return input.length >= 6;
-      },
-      "Password should be longer."
-    ]
-  },
-  // `email` must be of type String
-  // `email` must be unique
-  // `email` must match the regex pattern below and throws a custom error message if it does not
-  // You can read more about RegEx Patterns here https://www.regexbuddy.com/regex.html
-  email: {
-    type: String,
-    unique: true,
-    match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
-  },
-  // `date` must be of type Date. The default value is the current date
-  userCreated: {
-    type: Date,
-    default: Date.now
-  }
-});
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    synopsis: String,
+    date: { type: Date, default: Date.now }
+  });
+
 
 // This creates our model from the above schema, using mongoose's model method
-var User = mongoose.model("User", UserSchema);
+const Book = mongoose.model("Book", BookSchema);
 
 // Export the User model
-module.exports = User;
+module.exports = Book;
