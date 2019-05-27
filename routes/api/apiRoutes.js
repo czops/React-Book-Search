686941c6b974aa394ec5887848d9fced0c4edbc7
czops <37029225+czops@users.noreply.github.com>
2019-05-27 18:16:42 -0500

    messing with npm packages
    trying to make deployment to Heroku pass through

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -4,7 +4,7 @@ const router = require("express").Router();
 
 router.get("/books", (req, res) => {
     axios
-        .get("https://www.googleapis.com/books/v1/volumes?q="+ params.title + "&key=" + "AIzaSyDY6hJYflIh2uvhhGH4HNamaGLhguBmLkI")
+        .get("https://www.googleapis.com/books/v1/volumes?q="+ req.params.title + "&key=" + "AIzaSyDY6hJYflIh2uvhhGH4HNamaGLhguBmLkI")
         
         .then(({ data: { results } }) => res.json(results))
         .catch(err => res.status(422).json(err));
